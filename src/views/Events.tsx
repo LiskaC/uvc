df1afb7298980ab0fe744a836d355116221c91b8
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2025-03-04 21:21:05 +0100

    FE-9: Create Google Calendar view MVP

--- src/views/Events.tsx
+++ src/views/Events.tsx
@@ -5,5 +5,5 @@ export const Events: FC = () => (
     <h1>Events</h1>
     <h2>Sundays 13:00 - 17:00, Wellington Statue</h2>
     <p>Join our weekly demo at Wellington statue</p>
-  </>
+    <iframe src="https://calendar.google.com/calendar/embed?src=cc4058d2c561f607f03105309f22efb95585edf7c71ea5ba47d59f0d2f5ad925%40group.calendar.google.com&ctz=Europe%2FBerlin" width="800" height="600" scrolling="no"></iframe>  </>
 )
