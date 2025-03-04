e47c1d4f58236addf7d622b526931560b6b7703d
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2025-03-04 20:58:39 +0100

    FE-8: Add links to view all PDF reports

--- src/views/Reports.tsx
+++ src/views/Reports.tsx
@@ -1,8 +1,25 @@
 import { FC } from 'react'
 
+import styles from './Reports.module.scss'
+
+const pdfs = [
+  { filename: 'this-is-a-pdf.pdf', text: 'Spending report 01.01.2025 - 31.01.2025' },
+  { filename: 'this-is-a-pdf-2.pdf', text: 'Spending report 01.02.2025 - 28.02.2025' }
+]
+
 export const Reports: FC = () => (
   <>
     <h1>Reports</h1>
     <p>Our monthly spending reports</p>
+    <p>Open pdfs:</p>
+    <ul className={styles.reportList}>
+      {pdfs.map(pdf => (
+        <li>
+          <a href={pdf.filename} id={pdf.filename} target='_blank' className={styles.reportLink}>
+            {pdf.text}
+          </a>
+        </li>
+      ))}
+    </ul>
   </>
 )
