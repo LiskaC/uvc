b5228dd71993a6c0b111941fb8ca461c2c0dc38c
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2025-03-09 22:24:35 +0100

    FE-32: Improve Volunteer section UI

--- src/sections/Volunteer/Volunteer.tsx
+++ src/sections/Volunteer/Volunteer.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import { routes } from '../../routes'
 
+import styles from './Volunteer.module.scss'
+
 interface Props {
   name: string
   description: string
@@ -11,7 +13,7 @@ interface Props {
 }
 
 const Opportunity: FC<Props> = props => (
-  <div>
+  <div className={styles['opportunity']}>
     <h3>{props.name}</h3>
     <p>{props.description}</p>
     <a href={props.link.address}>{props.link.text}</a>
@@ -30,10 +32,12 @@ export const Volunteer: FC = () => (
       <br/>
       Other ways people can volunteer with UVC.....
     </p>
-    <h2>Volunter with:</h2>
-    <Opportunity name='Edinburgh Spiders' description='Description: Weave camouflage nets.'
-      link={{ text: 'where to find timetable', address: '' }}/>
-    <Opportunity name='Help Ukraine Scotland' description='Lots of events and stuff.'
-      link={{ text: 'facebook group', address: 'https://www.facebook.com/groups/helpukrainescotland' }}/>
+    <h2>Volunteer with other groups</h2>
+    <div>
+      <Opportunity name='Edinburgh Spiders' description='Description: Weave camouflage nets.'
+        link={{ text: 'where to find timetable', address: '' }}/>
+      <Opportunity name='Help Ukraine Scotland' description='Lots of events and stuff.'
+        link={{ text: 'facebook group', address: 'https://www.facebook.com/groups/helpukrainescotland' }}/>
+    </div>
   </section>
 )
