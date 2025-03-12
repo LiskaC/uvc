5a31074c68af47438f50d568aeede4a9b5356edf
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2025-03-12 11:31:46 +0100

    FE-37: Add team gallery

--- src/sections/Team/Team.tsx
+++ src/sections/Team/Team.tsx
@@ -1,5 +1,21 @@
 import { FC } from 'react'
+import { team, Member } from './data'
+
+import styles from './Team.module.scss'
+
+const TeamMember: FC<{ member: Member }> = props => (
+  <div className={styles['team-member']}>
+    <img className={styles['team-member__photo']} src={props.member.photo} alt='team member'/>
+    <h4>{props.member.name}</h4>
+    <h4>{props.member.role}</h4>
+  </div>
+)
 
 export const Team: FC = () => (
-  <h2>Committee About</h2>
+  <section>
+    <h2>Our Team</h2>
+    <div className={styles['team']}>
+      {team.map(person => <TeamMember key={person.id} member={person}/>)}
+    </div>
+  </section>
 )
