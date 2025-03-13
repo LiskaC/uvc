2a9478a6df3609467629e0afd1b2657d479c5b7e
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2025-03-13 10:14:43 +0100

    FE-39: Added logos

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -5,6 +5,8 @@ import { Social } from '../Social/Social'
 import { Hamburger } from '../Hamburger/Hamburger'
 import { SideMenu } from '../../sections/SideMenu/SideMenu'
 
+import LOGO from '../../assets/icons/logo-light.svg'
+
 import styles from './AppShell.module.scss'
 
 export const AppShell: FC = () => {
@@ -29,7 +31,10 @@ const AppShellView: FC<Props> = props => (
       <div className={styles['app-shell__hamburger']}>
         <Hamburger onClick={props.toggleMenu} isOpen={props.isMenuOpen}/>
       </div>
-      <h1 className={styles['app-shell__title']}>UKRAINIAN VICTORY CAMPAIGN</h1>
+      <div className={styles['app-shell__logo']}>
+        <img className={styles['app-shell__icon']} src={LOGO} alt=''/>
+        <h1 className={styles['app-shell__title']}>UKRAINIAN VICTORY CAMPAIGN</h1>
+      </div>
       <div className={styles['app-shell__navigation']}><NavBar/></div>
       <div className={styles['app-shell__social']}><Social/></div>
     </header>
