7ff78aac20f5db20efb1e810a42378bf0252ed1c
Liska Crofts <yscrofts@hotmail.com>
2025-09-21 14:35:12 +0200

    FE-59: Finalise Donate page content

--- src/sections/Donate/Donate.tsx
+++ src/sections/Donate/Donate.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react'
 import { routes } from '../../routes'
 
+import PAYPAL from '../../assets/icons/paypal.png'
+import FUNDRAISING from '../../assets/images/cakes.jpg'
+
 import styles from './Donate.module.scss'
 
 export const Donate: FC = () => (
@@ -12,21 +15,21 @@ export const Donate: FC = () => (
     </p>
     <h2>Ways to donate:</h2>
     <div className={styles['donate__options']}>
-      <div>
+      <div className={styles['donate__option']}>
         <h3>PayPal</h3>
         <p>Donate in any currency, even if you don’t have a PayPal account</p>
-        <img src='src/assets/icons/paypal.png' alt='' aria-hidden />
+        <img className={styles['donate__image']} src={PAYPAL} alt='' aria-hidden />
         <a href='https://www.paypal.com/paypalme/UVC123' target='_blank' rel='noreferrer'>
           Donate via PayPal
         </a>
       </div>
-      <div>
+      <div className={styles['donate__option']}>
         <h3>At our demonstrations</h3>
         <p>
           <a href={routes.events.path}>Come to our Saturday demonstrations</a> and donate by cash or
           card
         </p>
-        <img src='src/assets/images/cakes.png' alt='' aria-hidden />
+        <img className={styles['donate__image']} src={FUNDRAISING} alt='' aria-hidden />
       </div>
     </div>
   </section>
